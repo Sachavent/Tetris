a25b034ec8577e0ef6662f788723b6bce95cad89
Sachavent <ventribout@et.esiea.fr>
2016-03-12 16:47:06 +0100

    Ajout interface Réseau
    Ajout de l'interface réseau

---
+++ org.esiea.Tetris/src/main/java/InterfaceReseau/Joueur.java
@@ -0,0 +1,27 @@
+package InterfaceReseau;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class Joueur {
+	// Fonction qui permet d'envoyer un message
+	public static void sendmessage(String message,Socket s, PrintWriter print) {
+        print.println(message);
+        print.flush();
+	}
+	
+	public static String recievemessage(BufferedReader recieve) throws IOException {
+		String message_distant=null;
+		try {
+        message_distant = recieve.readLine();
+		} catch (SocketException e) {
+			System.out.println("la connexion est déjà fermé");
+		}
+        System.out.println(message_distant);
+        return message_distant;
+        
+	}
+}
